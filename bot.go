0c7642ba6422c2092413ace3124b18d9718e577c
ilovelili <route666@live.cn>
2023-05-01 16:51:34 +0900

    add bot response entity

--- bot.go
+++ bot.go
@@ -5,6 +5,36 @@ import (
 	"fmt"
 )
 
+type BotResponse struct {
+	Bot
+	AI
+}
+
+type Bot struct {
+	BotUserID                   string            `json:"bot_userid"`
+	BotNickname                 string            `json:"bot_nickname"`
+	BotProfileURL               string            `json:"bot_profile_url"`
+	BotType                     string            `json:"bot_type"`
+	BotToken                    string            `json:"bot_token"`
+	BotMetadata                 map[string]string `json:"bot_metadata"`
+	BotCallbackURL              string            `json:"bot_callback_url"`
+	IsPrivacyMode               bool              `json:"is_privacy_mode"`
+	EnableMarkAsRead            bool              `json:"enable_mark_as_read"`
+	ShowMember                  bool              `json:"show_member"`
+	ChannelInvitationPreference int               `json:"channel_invitation_preference"`
+	CreatedAt                   int64             `json:"created_at"`
+}
+
+type AI struct {
+	Backend          string  `json:"backend"`
+	SystemMessage    string  `json:"system_message"`
+	Temperature      float64 `json:"temperature"`
+	MaxTokens        int     `json:"max_tokens"`
+	TopP             float64 `json:"top_p"`
+	PresencePenalty  float64 `json:"presence_penalty"`
+	FrequencyPenalty float64 `json:"frequency_penalty"`
+}
+
 type SendBotMessageRequest struct {
 	BotID        string   `json:"bot_userid"`
 	Message      string   `json:"message"`
